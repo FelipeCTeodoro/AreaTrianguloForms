56eef849d65573e0a12b03a162a111c521d85b19
FelipeCTeodoro <96137014+FelipeCTeodoro@users.noreply.github.com>
2023-10-27 20:57:55 -0300

    Interface e Código Concluídos
    Foi finalizada a interface e implementado o código com tratamentos de erro.

--- AreaTrianguloForms/Form1.Designer.cs
+++ AreaTrianguloForms/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txbBase = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbBase
+            // 
+            this.txbBase.Location = new System.Drawing.Point(470, 140);
+            this.txbBase.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txbBase.Name = "txbBase";
+            this.txbBase.Size = new System.Drawing.Size(164, 26);
+            this.txbBase.TabIndex = 0;
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(470, 205);
+            this.txbAltura.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(164, 26);
+            this.txbAltura.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(488, 305);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(125, 35);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.Enabled = false;
+            this.txbResultado.Location = new System.Drawing.Point(802, 140);
+            this.txbResultado.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.Size = new System.Drawing.Size(164, 26);
+            this.txbResultado.TabIndex = 3;
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Location = new System.Drawing.Point(381, 146);
+            this.lblBase.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(55, 20);
+            this.lblBase.TabIndex = 4;
+            this.lblBase.Text = "Base:";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(379, 205);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(62, 20);
+            this.lblAltura.TabIndex = 5;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(698, 146);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(96, 20);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1062, 692);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblBase);
+            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.txbBase);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Calculador de Área de Triângulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txbBase;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txbResultado;
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
 
--- AreaTrianguloForms/Form1.cs
+++ AreaTrianguloForms/Form1.cs
@@ -16,5 +16,33 @@ namespace AreaTrianguloForms
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obter os números do Form:
+                double B1, A2, resultado;
+
+                B1 = double.Parse(txbBase.Text);
+                A2 = double.Parse(txbAltura.Text);
+
+                // Fazendo E Mostrando no Campo Resultado A Operação:
+
+                resultado = B1 * A2 / 2;
+
+                txbResultado.Text = resultado.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Dados invalidos informados!");
+                // Limpar os txbs:
+                txbBase.Clear();
+                txbAltura.Clear();
+                txbResultado.Clear();
+            }
+            
+        }
+
     }
 }
